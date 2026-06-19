0b23956e73c0a9f21759c53c328035f15a023d8a
DEVANAND-JAYARAMAN <devanand3254@gmail.com>
2026-06-19 17:27:07 +0530

    changed baseURL from localhost to huggingfaceurl

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:8000' });
+const API = axios.create({
+  baseURL: 'https://devanand170904-smartroadai-potholedetection.hf.space'
+});
 
-export const predictImage = (formData) => API.post('/predict', formData);
+export const predictImage = (formData) =>
+  API.post('/predict', formData);
